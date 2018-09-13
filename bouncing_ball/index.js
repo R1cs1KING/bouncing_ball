7789af27679d63f0f7871ebbfdd98e2cefddcb52
richardtarnoczi <ricsi960413@gmail.com>
2018-09-13 12:59:18 +0200

    added changing speed, shadow with effect and rotation speed changes by speed

--- bouncing_ball/index.js
+++ bouncing_ball/index.js
@@ -2,7 +2,10 @@ let Application = PIXI.Application,
     Container = PIXI.Container,
     loader = PIXI.loader,
     Sprite = PIXI.Sprite;
+    Graphics = PIXI.Graphics;
 let app = new Application({width: 800, height: 1200});
+app.renderer.backgroundColor = 0xF6F8FA;
+let gu = new GameUtilities();
 
 document.body.appendChild(app.view);
 
@@ -12,23 +15,36 @@ loader
     .add("images/minus.png")
     .load(setup);
 
-let ball, plus, minus;
+//declaration of the variables    
+let ball, plus, minus, speed, direction, shadow;
 
 function setup() {
     speed = 10;
     direction = 1;
 
+    //intialization of the shadow
+    shadow = new Graphics();
+    shadow.beginFill(0x000000);
+    shadow.drawEllipse(0, 0, 200, 80);
+    shadow.endFill();
+    shadow.alpha = 0.1;
+    shadow.x = 400;
+    shadow.y = 1172;
+    app.stage.addChild(shadow);
+
+    //intialization of the ball
     ball = Sprite.fromImage("images/ball.png");
     console.log(ball.height);
     ball.y = 240
     ball.x = 400;
     ball.anchor.set(0.5);
-
     ball.vy = speed * direction;
     direction *= -1;
-
     app.stage.addChild(ball);
 
+    distanceBetweenSprites();
+
+    //intialization of the plus button
     plus = Sprite.fromImage("images/plus.png");
     plus.scale.set(0.5, 0.5);
     plus.y = 500;
@@ -37,11 +53,17 @@ function setup() {
     plus.buttonMode = true;
     app.stage.addChild(plus);
 
+    //adding functionality to the plus button
     plus.on('pointerdown', () => {
         speed += 10;
-        ball.vy = speed * direction;
+        if (speed <= 0) {
+            ball.vy = 0;
+        } else {
+            ball.vy = speed * direction;
+        }
     });
 
+    //intialization of the minus button
     minus = Sprite.fromImage("images/minus.png");
     minus.scale.set(0.5, 0.5);
     minus.y = 700;
@@ -50,14 +72,21 @@ function setup() {
     minus.buttonMode = true;
     app.stage.addChild(minus);
 
+    //adding functionality to the minus button
     minus.on('pointerdown', () => {
-        speed -= 10;
-        ball.vy = speed * direction;
+        if (speed <= 0) {
+            speed = 0;
+        } else {
+            speed -= 10;
+            ball.vy = speed * direction;
+        }
     });
     
     state = move;
 
     app.ticker.add(delta => loop(delta));
+    app.ticker.add(delta => shadowGrow(delta));
+    app.ticker.add(delta => velocityChange(delta));
 
 }
 
@@ -65,28 +94,33 @@ function loop(delta) {
     state(delta);
 }
 
+//function to handle the movements
 function move(delta) {
     ball.y += ball.vy;
 
-    let ballHitsBorder = contain(ball, {x: 28, y: 10, width: 800, height: 1200});
+    let ballHitsBorder = contain(ball, {x: 28, y: 0, width: 800, height: 1200});
 
     if (ballHitsBorder === "top" || ballHitsBorder === "bottom") {
+        //ball.vy *= -1;
+        ball.vy = velocityChange();
+    }
+
+    if (speed > 0) {
+        ball.rotation += 0.002 * speed * delta;
+    } else if (speed <= 0) {
+        ball.vy = 0;
+    }
+
+    if(ballHitsBorder === "bottom") {
+        ball.vy *= 1;
+    }
+
+    if(ballHitsBorder === "top") {
         ball.vy *= -1;
     }
-
-    if (speed !== 0) {
-        ball.rotation += 0.05 * delta;
-    }
-
-    /*if (ballHitsBorder === "top") {
-        speed += 10;
-    }
-
-    if (ballHitsBorder === "bottom") {
-        speed -= 10;
-    }*/
 }
 
+//function to make sure the ball is not going out of the stage
 function contain(sprite, container) {
     let collision = undefined;
       
@@ -102,4 +136,20 @@ function contain(sprite, container) {
 
       return collision;
 
+}
+
+//function to handle the growth of the shadow and its fading effect
+function shadowGrow(delta) {
+    shadow.alpha = 1 - (distanceBetweenSprites() / 5000);
+    shadow.scale.set(1.0 * distanceBetweenSprites() / 800, 1.0);
+}
+
+//function to handle the change of the velocity
+function velocityChange(delta) {
+    return (speed * (distanceBetweenSprites() / 1000)) * direction;
+}
+
+//function to calculate the distance between the shadow and the ball
+function distanceBetweenSprites() {
+    return gu.distance(ball, shadow);
 }
